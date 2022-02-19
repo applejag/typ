b55962b7ec3d1709c30d7fe7d8c128fa70ddff03
Kalle Fagerberg <kalle.f8@protonmail.com>
2022-02-19 13:15:04 +0100

    Added more sort functions

--- sort.go
+++ sort.go
@@ -9,30 +9,69 @@ import (
 	"sort"
 )
 
-// SortOrdered implements sort.Interface via the default less-than operator.
-type SortOrdered[T Ordered] []T
+type sortOrdered[T Ordered] []T
 
-func (s SortOrdered[T]) Len() int {
+func (s sortOrdered[T]) Len() int {
 	return len(s)
 }
 
-func (s SortOrdered[T]) Swap(i, j int) {
+func (s sortOrdered[T]) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
-func (s SortOrdered[T]) Less(i, j int) bool {
+func (s sortOrdered[T]) Less(i, j int) bool {
 	return s[i] < s[j]
+}
+
+type sortLess[T any] struct {
+	slice []T
+	less  func(a, b T) bool
+}
+
+func (s sortLess[T]) Len() int {
+	return len(s.slice)
+}
+
+func (s sortLess[T]) Swap(i, j int) {
+	s.slice[i], s.slice[j] = s.slice[j], s.slice[i]
+}
+
+func (s sortLess[T]) Less(i, j int) bool {
+	return s.less(s.slice[i], s.slice[j])
 }
 
 // Sort will sort a slice using the default less-than operator.
 func Sort[T Ordered](slice []T) {
-	sort.Sort(SortOrdered[T](slice))
+	sort.Sort(sortOrdered[T](slice))
+}
+
+// SortFunc will sort a slice using the given less function.
+func SortFunc[T any](slice []T, less func(a, b T) bool) {
+	sort.Sort(sortLess[T]{slice, less})
 }
 
 // SortDesc will sort a slice using the default less-than operator in
 // descending order.
 func SortDesc[T Ordered](slice []T) {
-	sort.Sort(sort.Reverse(SortOrdered[T](slice)))
+	sort.Sort(sort.Reverse(sortOrdered[T](slice)))
+}
+
+// SortDescFunc will sort a slice using the given less function in
+// descending order.
+func SortDescFunc[T any](slice []T, less func(a, b T) bool) {
+	sort.Sort(sort.Reverse(sortLess[T]{slice, less}))
+}
+
+// SortStableFunc will sort a slice using the given less function, while keeping
+// the original order of equal elements.
+func SortStableFunc[T any](slice []T, less func(a, b T) bool) {
+	sort.Stable(sortLess[T]{slice, less})
+}
+
+// SortStableDescFunc will sort a slice using the given less function in
+// descending order, while keeping the original order of equal elements.
+func SortStableDescFunc[T any](slice []T, less func(a, b T) bool) {
+	sort.Stable(sort.Reverse(sortLess[T]{slice, less}))
 }
 
 // Reverse will reverse all elements inside a slice, in place.
