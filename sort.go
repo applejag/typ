68f823d756bc62978afeb03bbbbeb50420687121
Kalle Fagerberg <kalle.f8@protonmail.com>
2022-02-19 14:26:38 +0100

    Added typ.SearchFunc

--- sort.go
+++ sort.go
@@ -41,41 +41,41 @@ func (s sortLess[T]) Less(i, j int) bool {
 }
 
 // Sort will sort a slice using the default less-than operator.
-func Sort[T Ordered](slice []T) {
-	sort.Sort(sortOrdered[T](slice))
+func Sort[S ~[]E, E Ordered](slice S) {
+	sort.Sort(sortOrdered[E](slice))
 }
 
 // SortFunc will sort a slice using the given less function.
-func SortFunc[T any](slice []T, less func(a, b T) bool) {
-	sort.Sort(sortLess[T]{slice, less})
+func SortFunc[S ~[]E, E any](slice S, less func(a, b E) bool) {
+	sort.Sort(sortLess[E]{slice, less})
 }
 
 // SortDesc will sort a slice using the default less-than operator in
 // descending order.
-func SortDesc[T Ordered](slice []T) {
-	sort.Sort(sort.Reverse(sortOrdered[T](slice)))
+func SortDesc[S ~[]E, E Ordered](slice S) {
+	sort.Sort(sort.Reverse(sortOrdered[E](slice)))
 }
 
 // SortDescFunc will sort a slice using the given less function in
 // descending order.
-func SortDescFunc[T any](slice []T, less func(a, b T) bool) {
-	sort.Sort(sort.Reverse(sortLess[T]{slice, less}))
+func SortDescFunc[S ~[]E, E any](slice S, less func(a, b E) bool) {
+	sort.Sort(sort.Reverse(sortLess[E]{slice, less}))
 }
 
 // SortStableFunc will sort a slice using the given less function, while keeping
 // the original order of equal elements.
-func SortStableFunc[T any](slice []T, less func(a, b T) bool) {
-	sort.Stable(sortLess[T]{slice, less})
+func SortStableFunc[S ~[]E, E any](slice S, less func(a, b E) bool) {
+	sort.Stable(sortLess[E]{slice, less})
 }
 
 // SortStableDescFunc will sort a slice using the given less function in
 // descending order, while keeping the original order of equal elements.
-func SortStableDescFunc[T any](slice []T, less func(a, b T) bool) {
-	sort.Stable(sort.Reverse(sortLess[T]{slice, less}))
+func SortStableDescFunc[S ~[]E, E any](slice S, less func(a, b E) bool) {
+	sort.Stable(sort.Reverse(sortLess[E]{slice, less}))
 }
 
 // Reverse will reverse all elements inside a slice, in place.
-func Reverse[T any](slice []T) {
+func Reverse[S ~[]E, E any](slice S) {
 	for i, j := 0, len(slice)-1; i < len(slice)/2; i, j = i+1, j-1 {
 		slice[i], slice[j] = slice[j], slice[i]
 	}
@@ -84,7 +84,7 @@ func Reverse[T any](slice []T) {
 // Shuffle will randomize the order of all elements inside a slice. It uses the
 // rand package for random number generation, so you are expected to have called
 // rand.Seed beforehand.
-func Shuffle[T any](slice []T) {
+func Shuffle[S ~[]E, E any](slice S) {
 	rand.Shuffle(len(slice), func(i, j int) {
 		slice[i], slice[j] = slice[j], slice[i]
 	})
@@ -93,7 +93,7 @@ func Shuffle[T any](slice []T) {
 // ShuffleRand will randomize the order of all elements inside a slice using the
 // Fisher-Yates shuffle algoritm. It uses the rand argument for random number
 // generation.
-func ShuffleRand[T any](slice []T, rand *rand.Rand) {
+func ShuffleRand[S ~[]E, E any](slice S, rand *rand.Rand) {
 	rand.Shuffle(len(slice), func(i, j int) {
 		slice[i], slice[j] = slice[j], slice[i]
 	})
@@ -103,8 +103,21 @@ func ShuffleRand[T any](slice []T, rand *rand.Rand) {
 // slice of ordered values. The index of the first match is returned, or the
 // index where it insert the value if the value is not present.
 // The slice must be sorted in ascending order.
-func Search[T Ordered](slice []T, value T) int {
+func Search[S ~[]E, E Ordered](slice S, value E) int {
 	return sort.Search(len(slice), func(i int) bool {
 		return slice[i] >= value
+	})
+}
+
+// SearchFunc performs a binary search to find the index of a value in a sorted
+// slice of ordered values. The index of the first match is returned, or the
+// index where it insert the value if the value is not present.
+// The slice must be sorted in ascending order.
+//
+// The less function should return true if the given value is less than the
+// sought value.
+func SearchFunc[S ~[]E, E any](slice S, less func(a E) bool) int {
+	return sort.Search(len(slice), func(i int) bool {
+		return !less(slice[i])
 	})
 }
